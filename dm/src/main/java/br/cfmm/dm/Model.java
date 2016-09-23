3da9109e0256e830f075d302e04aa253dc788523
cristiano <cristiano.macedo@embrapa.br>
2016-09-23 14:56:22 -0300

    V0

---
+++ dm/src/main/java/br/cfmm/dm/Model.java
@@ -0,0 +1,50 @@
+package br.cfmm.dm;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cristiano Francis Matos de Macedo
+ */
+public class Model {
+    private String texto;
+    private String tipo;
+    
+    public Model ( String _texto, String _tipo )
+    {
+        this.texto = _texto;
+        this.tipo = _tipo;
+    }
+
+    /**
+     * @return the texto
+     */
+    public String getTexto() {
+        return texto;
+    }
+
+    /**
+     * @param texto the texto to set
+     */
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
